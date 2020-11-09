a802dc1012a82fe3f0db94f7aa8dcc202b5974fa
Viktor Forsberg <viktor.forsberg@skola.botkyrka.se>
2020-11-09 15:58:55 +0100

    WIP

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const MAX_NUMBER = 100
 const MIN_NUMBER = 0
 
-let guess
+let guess 
 
 document.getElementById('too-high-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too high?
@@ -13,6 +13,10 @@ document.getElementById('too-low-btn').addEventListener('click', function () {
     
 })
 
+document.getElementById('is-correct-btn').addEventListener('click', function () {
+setMessage('Woho! :)')
+    
+})
 
 function setMessage(msg) {
     document.getElementById('message').innerText = msg
